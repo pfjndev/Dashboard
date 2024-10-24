86b18139cdd3151b8d0fcb01fb27ffecce4fa9e5
pfjndev <88943477+pfjndev@users.noreply.github.com>
2024-10-24 17:03:47 +0100

    feature: add layout panels.

--- src/dashboard/Dashboard.java
+++ src/dashboard/Dashboard.java
@@ -1,150 +1,133 @@
 package dashboard;
 
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.WindowConstants;
-import javax.swing.JMenuBar;
-import java.awt.Dimension;
-import java.awt.Frame;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import javax.swing.*;
+import java.awt.*;
+
+import dashboard.panels.FacesPanel;
+import dashboard.panels.ProductNamePanel;
+import dashboard.panels.KPIAvgRespTimePanel;
+import dashboard.panels.CSATOverMonthAreaChart;
+import dashboard.panels.CircularCSATPanel;
+import dashboard.panels.CustomerEffortGaugePanel;
+import dashboard.panels.NPSGaugePanel;
+import dashboard.panels.SatisfactionBreakdownChartPanel;
 
 public class Dashboard extends JFrame {
 
-    /** createAndShowGui method to run the dashboard.
-     * 
-     * The createAndShowGui method will create a new JFrame object and set the title of the dashboard.
-     * The default close operation is set to exit the application.
-     * The size of the dashboard is set to full screen.
-     * 
-     * A menu bar is added to the dashboard.
-     * 
-     * Menus: Dashboard, File, Edit, View, Settings.
-     * 
-     * Menu items:
-     * 
-     * Dashboard - About, Help, Check For Updates, Exit.
-     * 
-     * File - New File, Open File, Save File, Save File As, Close.
-     * 
-     * Edit - Copy, Cut, Paste, Delete.
-     * 
-     * View - Zoom In, Zoom Out, Full Screen.
-     * 
-     * Settings - Tools, Customize, Preferences.
+    private static final String FONT_NAME = "Arial";
+    private static final int FONT_WEIGHT = Font.BOLD;
+    private static final int FONT_SIZE = 24;
+    private static final Font FONT = new Font(FONT_NAME, FONT_WEIGHT, FONT_SIZE);
+    
+    private static final Color FOREGROUND_COLOR = Color.WHITE;
+    private static final Color BACKGROUND_COLOR = new Color(0x1F2A44);
+    
+    private static final String APP_TITLE = "Support Team Slide Dashboard";
 
-    * The main panel is added to the frame, then the pack method is called
-    * to resize the frame to fit the panel and finally the frame is set to visible.
+    public Dashboard() {
+        setTitle("PFJN - 1705125");               // Set the title of the dashboard
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the application when clicking the close button
+        setExtendedState(JFrame.MAXIMIZED_BOTH);        // Fullscreen
+        setLayout(new GridBagLayout());                 // Use GridBagLayout for the dashboard layout
 
-    * @param args The command line arguments.
-    * @version 1.0
-    * @author Pedro Nunes, 1705125
-    * @return 
-    **/
-    private static void createAndShowGui() {
-        // Create a new JFrame object.
-        JFrame frame = new Dashboard();
-        // Set the title of the dashboard.
-        frame.setTitle("Dashboard - PFJN - 1705125");
-        // Set the default close operation to exit the application.
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        // Set the dashboard to full screen.
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-        // Set the size of the dashboard.
-        Dimension fullScreen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setPreferredSize(fullScreen);
-        
-        // Set the layout of the dashboard to GridBagLayout.
-        frame.setLayout(new GridBagLayout());
+        getMbMenu(this);
+
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;                              // Column
-        gbc.gridy = 0;                              // Row
-        gbc.weightx = 1.0;                          // Weight of the column
-        gbc.weighty = 1.0;                          // Weight of the row
-        gbc.fill = GridBagConstraints.BOTH;         // Fill the column and row
-        gbc.anchor = GridBagConstraints.CENTER;     // Center the column and row
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
 
+        // Application Title
+        JLabel titleLabel = new JLabel(APP_TITLE, SwingConstants.CENTER);
+        titleLabel.setFont(FONT);                   // Custom font
+        titleLabel.setBackground(BACKGROUND_COLOR); // Dark background
+        titleLabel.setOpaque(true);        // Ensure background color is visible
+        titleLabel.setForeground(FOREGROUND_COLOR); // White text
+        gbc.gridwidth = 4;                          // Span across all columns
+        gbc.gridx = 0;                              // Start at first column
+        gbc.gridy = 0;                              // First row
+        gbc.weighty = 0.1;                          // Title takes 10% of the height
+        add(titleLabel, gbc);                       // Add title to the dashboard panel
+
+        gbc.gridwidth = 1;                          // Reset grid width for further components
+        gbc.weighty = 1;                            // Reset weight for the rest of the components
+
+        // First Row
+        add((new ProductNamePanel()), gbc(0, 1));           // First element
+        add((new KPIAvgRespTimePanel()), gbc(1, 1));   // Second element
+        add((new CSATOverMonthAreaChart()), gbc(2, 1));     // Third element
+        gbc.gridheight = 2;                                     // Last element spans 2 rows
+        add((new FacesPanel()), gbc(3, 1));                 // Fourth element
+
+        // Reset gridheight for the second row
+        gbc.gridheight = 1;
+
+        // Second Row
+        add(new CircularCSATPanel(), gbc(0, 2));            // First element
+        add(new CustomerEffortGaugePanel(), gbc(1, 2));     // Second element
+        add(new NPSGaugePanel(), gbc(2, 2));                // Third element
+
+        // Third Row
+        add(new SatisfactionBreakdownChartPanel(), gbc(0, 3)); // First element
+        add(new KPIAvgRespTimePanel(), gbc(1, 3));        // Second element
+        add(new CSATOverMonthAreaChart(), gbc(2, 3));          // Third element
+        
+        pack();
+        setVisible(true);
+    }
+
+    private GridBagConstraints gbc(int x, int y) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+        gbc.weighty = 1;
+        return gbc;
+    }
+
+    private static void getMbMenu(JFrame frame) {
         // Add a menu bar to the dashboard.
         JMenuBar mb = new JMenuBar();
-        mb.setPreferredSize(new Dimension(fullScreen.width, 30));
         frame.setJMenuBar(mb);
 
-        // Add menus to the menu bar.
-        // Add a main menu.
-        JMenu mbMenu = new JMenu("Dashboard");
-        mb.add(mbMenu);
-        // Add menu items to the help menu.
-        mbMenu.add("About");
-        mbMenu.add("Help");
-        mbMenu.add("Check For Updates");
-        mbMenu.add("Exit");
-        
-        // Add a file menu.
-        mbMenu = new JMenu("File");
-        mb.add(mbMenu);
-        // Add menu items to the file menu.
-        mbMenu.add("New File");
-        mbMenu.add("Open File");
-        mbMenu.add("Save File");
-        mbMenu.add("Save File As");
-        mbMenu.add("Close");
+        // Add menus and menu items
+        JMenu dashboardMenu = new JMenu(APP_TITLE);
+        mb.add(dashboardMenu);
+        dashboardMenu.add("About");
+        dashboardMenu.add("Help");
+        dashboardMenu.add("Check For Updates");
+        dashboardMenu.add("Exit");
 
-        // Add a edit menu.
-        mbMenu = new JMenu("Edit");
-        mb.add(mbMenu);
-        // Add menu items to the edit menu.
-        mbMenu.add("Copy");
-        mbMenu.add("Cut");
-        mbMenu.add("Paste");
-        mbMenu.add("Delete");
+        JMenu fileMenu = new JMenu("File");
+        mb.add(fileMenu);
+        fileMenu.add("New File");
+        fileMenu.add("Open File");
+        fileMenu.add("Save File");
+        fileMenu.add("Save File As");
+        fileMenu.add("Close");
 
-        // Add a view menu.
-        mbMenu = new JMenu("View");
-        mb.add(mbMenu);
-        // Add menu items to the view menu.
-        mbMenu.add("Zoom In");
-        mbMenu.add("Zoom Out");
-        mbMenu.add("Full Screen");
+        JMenu editMenu = new JMenu("Edit");
+        mb.add(editMenu);
+        editMenu.add("Copy");
+        editMenu.add("Cut");
+        editMenu.add("Paste");
+        editMenu.add("Delete");
 
-        // Add a tools menu.
-        mbMenu = new JMenu("Settings");
-        mb.add(mbMenu);
-        // Add menu items to the settings menu.
-        mbMenu.add("Tools");
-        mbMenu.add("Customize");
-        mbMenu.add("Preferences");
+        JMenu viewMenu = new JMenu("View");
+        mb.add(viewMenu);
+        viewMenu.add("Zoom In");
+        viewMenu.add("Zoom Out");
+        viewMenu.add("Full Screen");
 
-        /**
-        * TODO - Add all menus and menu items kekyboard shortcuts.
-        * TODO - Add all menus and menu items eventListeners.
-        **/
-
-        /** Grid layout
-        * TODO - Add all panels to the grid layout.
-        *
-        *JPanel lineGraphPanel = new LineGraphPanel();
-        *JPanel barChartPanel = new BarChartPanel();
-        *JPanel pieChartPanel = new PieChartPanel();
-        *JPanel tablePanel = new TablePanel();
-        *JPanel mapPanel = new MapPanel();
-        *
-        * TODO - Add all panels to the frame.
-        *frame.getContentPane().add(lineGraphPanel, gbc);
-        *frame.getContentPane().add(barChartPanel, gbc);
-        *frame.getContentPane().add(pieChartPanel, gbc);
-        *frame.getContentPane().add(tablePanel, gbc);
-        *frame.getContentPane().add(mapPanel, gbc);
-        */
-        
-        // Display the frame.
-        frame.pack();
-        frame.setResizable(true);
-        frame.setLocationByPlatform(true);
-        frame.setVisible(true);
+        JMenu settingsMenu = new JMenu("Settings");
+        mb.add(settingsMenu);
+        settingsMenu.add("Tools");
+        settingsMenu.add("Customize");
+        settingsMenu.add("Preferences");
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> createAndShowGui());
+        SwingUtilities.invokeLater(() -> new Dashboard());
     }
 }
